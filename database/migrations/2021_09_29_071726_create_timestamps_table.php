da777333d9959d7555eb23dd63bdb345d948f172
dinawidyo <90294425+dinawidyo@users.noreply.github.com>
2021-10-01 09:00:31 +0700

    menambahkan migration

---
+++ database/migrations/2021_09_29_071726_create_timestamps_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimestampsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timestamps', function (Blueprint $table) {
+            $table->id();
+            $table->enum('aksi',['perbarui','hapus']);
+            $table->string('akun');
+            $table->string('objek');
+            $table->enum('role',['admin','user']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timestamps');
+    }
+}
