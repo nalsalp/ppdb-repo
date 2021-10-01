d05ea374a56d77b7d98c0579c8208d1a67ae28e8
Lead1n6 <cakracakara734@gmail.com>
2021-10-01 09:07:44 +0700

    aploud login register

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+ 
+
+Route::get('/', 'AuthController@showFormLogin')->name('login');
+Route::get('login', 'AuthController@showFormLogin')->name('login');
+Route::post('login', 'AuthController@login');
+Route::get('register', 'AuthController@showFormRegister')->name('register');
+Route::post('register', 'AuthController@register');
+ 
+Route::group(['middleware' => 'auth'], function () {
+ 
+    Route::get('home', 'HomeController@index')->name('home');
+    Route::get('logout', 'AuthController@logout')->name('logout');
+ 
+});
+
+
+
