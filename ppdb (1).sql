bb4bdb3e0a9a3319a7849bf2d9b5f55fae2c6b8c
dinawidyo <90294425+dinawidyo@users.noreply.github.com>
2021-10-05 09:13:54 +0700

    file sql dina

---
+++ ppdb (1).sql
@@ -0,0 +1,304 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 05, 2021 at 04:13 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ppdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_siswa`
+--
+
+CREATE TABLE `data_siswa` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_leng` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama_pang` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jk` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tempat_lahir` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `alamat_ktp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat_tinggal` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `asal_sklh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `j_1` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `j_2` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kewarganegaraan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `agama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tinggi_badan` int(11) NOT NULL,
+  `berat_badan` int(11) NOT NULL,
+  `umur` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `riwayat_peny` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cita_cita` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hobi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_telp` int(11) NOT NULL,
+  `anak_ke` int(11) NOT NULL,
+  `status_anak` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gol_darah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `suku` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prestasi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bakat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pel_disukai` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nik` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nisn` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ijazah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `akta` blob NOT NULL,
+  `kk` blob NOT NULL,
+  `foto_diri` blob NOT NULL,
+  `foto_rumah` blob NOT NULL,
+  `foto_kel` blob NOT NULL,
+  `kis` blob NOT NULL,
+  `kip` blob NOT NULL,
+  `sks` blob NOT NULL,
+  `skb` blob NOT NULL,
+  `spp_sek` blob NOT NULL,
+  `sktm` blob NOT NULL,
+  `biaya_saudara` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama_ayh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama_ibu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pek_ayh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pek_ibu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `asal_ayh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `asal_ibu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pend_ayh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pend_ibu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tempat_lahir_ayh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tempat_lahir_ibu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tgl_lahir_ayh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tgl_lahir_ibu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `agama_ayh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `agama_ibu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usia_ayh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usia_ibu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_telp_ibu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_telp_ayh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `peng_ayh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `peng_ibu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(5, '2021_09_29_070829_create_user_table', 3),
+(7, '2014_10_12_000000_create_users_table', 4),
+(8, '2014_10_12_100000_create_password_resets_table', 4),
+(9, '2019_08_19_000000_create_failed_jobs_table', 4),
+(10, '2021_09_29_042056_create_data_siswa_table', 4),
+(11, '2021_09_29_071726_create_timestamps_table', 4),
+(12, '2021_10_04_073229_add_fto_bukti_to_users', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timestamps`
+--
+
+CREATE TABLE `timestamps` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `aksi` enum('perbarui','hapus') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `akun` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `objek` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('admin','user') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_telp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `con_pass` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('admin','user') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('sementara','tetap') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_telp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('admin','user') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('sementara','tetap') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `fto_bukti` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `no_telp`, `role`, `status`, `remember_token`, `created_at`, `updated_at`, `fto_bukti`) VALUES
+(5, 'Gfgfdfgd', 'gfhjhgfd@gmail.com', '2021-10-04 00:49:56', '$2y$10$6h7dwj69lL/5nzXq3qhiQ.7pcqcTV8QD38RAAdSKYyF9J.k1/mROG', '123', 'admin', 'sementara', NULL, '2021-10-04 00:49:56', '2021-10-04 00:49:56', 'logo.png'),
+(6, 'Dina', 'din@gmail.com', '2021-10-04 01:30:29', '$2y$10$2y88UAbRc6uUhLo/DSqTJOE.KJ6.rpERaxx8qS.fdHwMfHOqDX3WG', '123', 'admin', 'sementara', NULL, '2021-10-04 01:30:29', '2021-10-04 01:30:29', 'C:\\xampp\\tmp\\php3911.tmp');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_siswa`
+--
+ALTER TABLE `data_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `timestamps`
+--
+ALTER TABLE `timestamps`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_siswa`
+--
+ALTER TABLE `data_siswa`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `timestamps`
+--
+ALTER TABLE `timestamps`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
